ee1be8ccdcb60f541acaa3fe50b40e491e15b02b
Adolfo Marinucci <adospace@msn.com>
2025-05-10 13:53:06 +0200

    downgrade system.text.json version, removed http extensions

--- src/DevInbox/DevInboxClient.cs
+++ src/DevInbox/DevInboxClient.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
-using System;
+﻿using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Reflection;
